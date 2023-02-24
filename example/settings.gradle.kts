59f7dbdcdeee632de5c3cbf1b4e2552af781edde
matrixdev <w>
2023-02-24 19:20:59 +0200

    changed example module name

--- example/settings.gradle.kts
+++ example/settings.gradle.kts
@@ -1,7 +1,9 @@
+rootProject.name = "android-rust-example"
+
+include(":app")
+
 includeBuild("..") {
     dependencySubstitution {
         substitute(module("dev.matrix.android-rust:plugin")).using(project(":plugin"))
     }
 }
-
-include(":app")
