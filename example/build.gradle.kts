f44516583dc41817a614b41336c70893f4b43ce5
Rostyslav Lesovyi <www.matrix.dev@gmail.com>
2024-12-27 14:46:41 +0200

    [#5] Update project structure and gradle version (#6)

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,28 +1,6 @@
-buildscript {
-    val kotlinVersion by extra { "1.7.0" }
-
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("io.github.MatrixDev.android-rust:plugin:*")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
-    rootProject.childProjects.values.forEach { project ->
-        delete(project.buildDir)
-    }
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.rust) apply false
 }
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "android-rust"
-
-include(":plugin")
